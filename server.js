f31bf0d79a6a5bdefe8eaa77bd03f0dab95dd6ad
MDeadwiler <mdeadwiler@email.com>
2024-09-26 12:01:06 -0400

    fruits-app

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", async(req,res) => {
-    res.send("hello");
-})
+// server.js
+
+// GET /
+app.get("/", async (req, res) => {
+    res.render("index.ejs");
+  });
+  
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
