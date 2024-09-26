04d0661284ac1342780c4fe3bc25815d6a978fba
MDeadwiler <mdeadwiler@email.com>
2024-09-26 12:14:40 -0400

    fruits-app

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-// Here is where we import modules
-// We begin by loading Express
+// server.js
+const dotenv = require("dotenv");
+dotenv.config();
+//the other way to bring in dotenv
+//require(dotenv).config();
+const mongoose = require("mongoose"); //require package
 const express = require("express");
 
 const app = express();
 
-// server.js
+// Connect to MongoDB using the connection string in the .env file
+mongoose.connect(process.env.MONGODB_URI);
+// log connection status to terminal on start
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+
 
 // GET /
 app.get("/", async (req, res) => {
